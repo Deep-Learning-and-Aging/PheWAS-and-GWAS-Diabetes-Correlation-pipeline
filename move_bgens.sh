bcdf8989d501a3517996d2aef1fc95b6a500fd04
Taykhoom Dalal <tdalal@g.ucla.edu>
2022-06-16 18:38:35 -0400

    print statements

--- move_bgens.sh
+++ move_bgens.sh
@@ -11,10 +11,12 @@ list+=(Y)
 list+=(XY)
 
 for i in "${list[@]}"; do
+
+    echo "start copying chr${i}_v3.bgen"
+
     cp /n/groups/patel/uk_biobank/project_52887_genetics/ukb_imp_chr${i}_v3.bgen /n/scratch3/users/t/tad368/geno/chr${i}_v3.bgen
 
-    echo "copied"
-
-    echo "chr${i}_v3.bgen"
+    echo "finished copying chr${i}_v3.bgen"
+    echo ""
 
 done
