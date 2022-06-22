aade3323645c42d76e5404012f00683588b3ea5f
Taykhoom Dalal <tdalal@g.ucla.edu>
2022-06-22 12:25:32 -0400

    code didn't add tabs properly + put data in wrong dir, fixed

--- utility_scripts/grab_IDS_Alan.sh
+++ utility_scripts/grab_IDS_Alan.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-ls /n/groups/patel/Alan/Aging/Medical_Images/images/Abdomen/Liver/Raw/ | cut -d'_' -f1 | sort -un > ../temp.txt
+ls /n/groups/patel/Alan/Aging/Medical_Images/images/Abdomen/Liver/Raw/ | cut -d'_' -f1 | sort -un > ../in_pop_IDs/temp.txt
 
-awk -F'\t' '{print $0" "$0}' ../temp.txt > ../Liver_in_sample_pop_IDs.txt
+awk -F' ' '{print $0" "$0}' ../in_pop_IDs/temp.txt > ../in_pop_IDs/Liver_in_sample_pop_IDs.txt
 
-ls /n/groups/patel/Alan/Aging/Medical_Images/images/Abdomen/Pancreas/Raw/ | cut -d'_' -f1 | sort -un > ../temp.txt
+ls /n/groups/patel/Alan/Aging/Medical_Images/images/Abdomen/Pancreas/Raw/ | cut -d'_' -f1 | sort -un > ../in_pop_IDs/temp.txt
 
-awk -F'\t' '{print $0" "$0}' ../temp.txt > ../Pancreas_in_sample_pop_IDs.txt
+awk -F' ' '{print $0" "$0}' ../in_pop_IDs/temp.txt > ../in_pop_IDs/Pancreas_in_sample_pop_IDs.txt
 
-rm ../temp.txt
+rm ../in_pop_IDs/temp.txt
 
 # sed removes weird <<<<<<< line from file that is added, not sure why its added
-cat ../Pancreas_in_sample_pop_IDs.txt ../Liver_in_sample_pop_IDs.txt | sort -un | sed '1d' > ../Abdomen_in_sample_pop_IDs.txt
+cat ../in_pop_IDs/Pancreas_in_sample_pop_IDs.txt ../in_pop_IDs/Liver_in_sample_pop_IDs.txt | sort -un | sed '1d' > ../in_pop_IDs/Abdomen_in_sample_pop_IDs.txt
+
+sed -e 's/ /\t/g' ../in_pop_IDs/Liver_in_sample_pop_IDs.txt > temp1.txt
+sed -e 's/ /\t/g' ../in_pop_IDs/Pancreas_in_sample_pop_IDs.txt > temp2.txt
+sed -e 's/ /\t/g' ../in_pop_IDs/Abdomen_in_sample_pop_IDs.txt > temp3.txt
+
+mv temp1.txt ../in_pop_IDs/Liver_in_sample_pop_IDs.txt
+mv temp2.txt ../in_pop_IDs/Pancreas_in_sample_pop_IDs.txt
+mv temp3.txt ../in_pop_IDs/Abdomen_in_sample_pop_IDs.txt
