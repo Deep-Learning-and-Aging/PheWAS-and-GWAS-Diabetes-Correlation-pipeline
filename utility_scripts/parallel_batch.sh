5825c993a4ced4b7952a06918885329251de7ad7
Taykhoom Dalal <tdalal@g.ucla.edu>
2022-06-22 11:05:21 -0400

    loop to convert bgen to bim/bam/fam on cluster

---
+++ utility_scripts/parallel_batch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+list=()
+
+for i in {1..22}; do
+    list+=($i)
+done
+
+list+=("X")
+
+for i in "${list[@]}"; do
+
+    echo "started script for chr${i}_v3_bfile"
+
+    sbatch ./convert_bgen_to_bfile_parallel.sh $i
+
+done
