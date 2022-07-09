b8a92f28ba7bf475b43aa73742cec542de3d693d
TaykhoomDalal <tdalal@g.ucla.edu>
2022-07-09 14:09:43 -0400

    fixed output log name

--- PheWAS_pipeline/4_fix_covariate_data.sh
+++ PheWAS_pipeline/4_fix_covariate_data.sh
@@ -3,8 +3,8 @@
 #SBATCH -t 0-3:00:00                                                                                # Runtime in D-HH:MM format
 #SBATCH -p short                                                                                    # Partition to run in
 #SBATCH --mem=64000M                                                                                # Memory total in MiB (for all cores)
-#SBATCH -o /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/batch_logs/make_pheno_%j.out  # File to which STDOUT will be written, including job ID (%j)
-#SBATCH -e /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/batch_logs/make_pheno_%j.err  # File to which STDERR will be written, including job ID (%j)
+#SBATCH -o /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/batch_logs/fix_covariate_data_%j.out  # File to which STDOUT will be written, including job ID (%j)
+#SBATCH -e /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/batch_logs/fix_covariate_data_%j.err  # File to which STDERR will be written, including job ID (%j)
 
 python3 /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/PheWAS_pipeline/4_fix_covariate_data.py \
         --pheno_files /home/tad368/data_dir/pheno/Abdomen_subsetted_pheno.tab \
